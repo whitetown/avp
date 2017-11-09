fca85537eb30627cf641e7cd8eb6a32acc717c08
Sergey Chehuta <whitetownmail@gmail.com>
2017-11-09 14:13:55 +0100

    Initial Commit

---
+++ AVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVP
+//
+//  Created by Sergey Chehuta on 09/11/2017.
+//  Copyright © 2017 WhiteTown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
